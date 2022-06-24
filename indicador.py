e3062962b98db4481e4cc085718e746ec07c9816
Matheus Trindade <emailmartingcf1@gmail.com>
2022-06-24 19:42:37 -0300

    x

--- indicador.py
+++ indicador.py
@@ -78,8 +78,8 @@ def get_sinal():
 
 				open(arq_sinais,'w').write(file.replace(sinal,''))
 
-	#par_ = sinal_[1]
-	#timeframe_ = sinal_[3]
+	par_ = sinal_[1]
+	timeframe_ = sinal_[3]
 	velas = API.get_candles(par, (int(timeframe) * 60), 20,  time.time())
 	ultimo = round(velas[0]['close'], 6)
 	primeiro = round(velas[-1]['close'], 6)
